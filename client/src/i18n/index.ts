944afc2c614cd24141238f1671983e208e6f9b65
Mahmoud Salah Elsebaey <91635205+mahmoudSElsebaey@users.noreply.github.com>
2026-08-21 17:02:12 +0300

    feat(i18n): TS locale modules (en/ar) and i18n index imports

--- client/src/i18n/index.ts
+++ client/src/i18n/index.ts
@@ -2,8 +2,8 @@ import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
 
-import en from './locales/en/translation.json'
-import ar from './locales/ar/translation.json'
+import en from './locales/en'
+import ar from './locales/ar'
 
 export const SUPPORTED_LANGS = ['en', 'ar'] as const
 export type AppLanguage = (typeof SUPPORTED_LANGS)[number]
